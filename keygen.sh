81846a49260ddc6f62d7b03060f405167a3d25d8
root <d.danielecapponi@gmail.com>
2020-06-14 18:44:32 +0200

    added -n option

--- keygen.sh
+++ keygen.sh
@@ -5,22 +5,42 @@ ALPHA=ABCDEFGHIJKLMNOPQRSTUVWXYZ
 symbols='.:-_#@[+]|!^?"<>£$%&/()='
 
 length=16
+keynum=1
 human=false
 
-next=''
+keygen() {
+	dictionary=$1
+	dictlen=$(echo -ne $dictionary | wc -c)
+	let dictlen--
+	let dictlen--
+	for s in $(seq 1 $length); do
+        c=$(shuf -i 0-$dictlen -n 1)
+        echo -ne ${dictionary[@]:$c:1}
+	done
+	echo
+}
+
+
 for opt in $@; do
         if [[ $next == '-l' ]]; then
-                length=$opt;
-        fi
+            length=$opt
+			next=''
+        elif [[ $next == '-n' ]]; then
+			keynum=$opt
+			next=''
+		fi
 
         if [[ $opt == '-h' || $opt == '--human' ]]; then
                 human=true
         elif [[ $opt == '-l' || $opt == '--length' ]]; then
                 next='-l'
-        elif [[ $opt == '?' || $opt == '--help' ]]; then
+        elif [[ $opt == '-n' || $opt == '--number' ]]; then
+				next='-n'
+		elif [[ $opt == '?' || $opt == '--help' ]]; then
                 echo -e "Random Key Generator"
                 echo -e "\t-l, --length      Specify key length, default is 16"
-                echo -e "\t-h, --human       Human-readable key, no symbols"
+                echo -e "\t-n, --number      Specify how many keys you want to create"
+				echo -e "\t-h, --human       Human-readable key, no symbols"
                 echo -e "\t ?, --help        Print this help message"
                 exit
         fi
@@ -31,12 +51,6 @@ if [[ $human == true ]]; then
         dictionary="$alpha$ALPHA$numbers$numbers$ALPHA$alpha"
 fi
 
-dictlen=$(echo -ne $dictionary | wc -c)
-let dictlen--
-let dictlen--
-for s in $(seq 1 $length); do
-        c=$(shuf -i 0-$dictlen -n 1)
-        echo -ne ${dictionary[@]:$c:1}
+for j in $(seq 1 $keynum); do
+	keygen $dictionary
 done
-
-echo
